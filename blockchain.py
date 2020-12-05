9ee440447485447362c21b226d4d75b208abc831
Albin Lundin <75508593+ChrisMB3@users.noreply.github.com>
2020-12-05 02:36:27 +0100

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,3 @@
-
-
 from Block import Block
 blockchain = []
 
@@ -8,12 +6,6 @@ genesis_block = Block("Block1", ["Block2"])
 second_block = Block(genesis_block.block_hash, ["Block3"])
 
 third_block = Block(second_block.block_hash, ["Block4"])
-print("Block hash: Genesis block")
-print(genesis_block.block_hash)
-
-print("Block hash: Second block")
-print(second_block.block_hash)
 
 print("Block hash: Third block")
 print(third_block.block_hash)
-
